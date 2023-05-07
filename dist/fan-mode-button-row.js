765e58c50cb3c3a1c0214c9255a1e2e7b7b6fa99
finity69x2 <32221243+finity69x2@users.noreply.github.com>
2023-05-07 05:49:31 -0400

    Update fan-mode-button-row.js

--- dist/fan-mode-button-row.js
+++ dist/fan-mode-button-row.js
@@ -6,115 +6,31 @@ window.customCards.push({
   preview: false,
 });
 
-class CustomFanModeRow extends Polymer.Element {
+const LitElement = customElements.get("ha-panel-lovelace") ? Object.getPrototypeOf(customElements.get("ha-panel-lovelace")) : Object.getPrototypeOf(customElements.get("hc-lovelace"));
+const html = LitElement.prototype.html;
+const css = LitElement.prototype.css;
 
-	static get template() {
-		return Polymer.html`
-			<style is="custom-style" include="iron-flex iron-flex-alignment"></style>
-			<style>
-				:host {
-					line-height: inherit;
-				}
-				.mode {
-					margin-left: 2px;
-					margin-right: 2px;
-					background-color: #759aaa;
-					border: 1px solid lightgrey; 
-					border-radius: 4px;
-					font-size: 10px !important;
-					color: inherit;
-					text-align: center;
-					float: right !important;
-					padding: 1px;
-					cursor: pointer;
-				}
-				
-				</style>
-					<hui-generic-entity-row hass="[[hass]]" config="[[_config]]">
-						<div class='horizontal justified layout' on-click="stopPropagation">
-							<button
-								class='mode'
-								style='[[_leftColor]];min-width:[[_width]];max-width:[[_width]];height:[[_height]];[[_hideLeft]]'
-								toggles name="[[_leftName]]"
-								on-click='setMode'
-								disabled='[[_leftState]]'>[[_leftText]]</button>
-							<button
-								class='mode'
-								style='[[_midLeftColor]];min-width:[[_width]];max-width:[[_width]];height:[[_height]];[[_hideMidLeft]]'
-								toggles name="[[_midLeftName]]"
-								on-click='setMode'
-								disabled='[[_midLeftState]]'>[[_midLeftText]]</button>
-							<button
-								class='mode'
-								style='[[_midRightColor]];min-width:[[_width]];max-width:[[_width]];height:[[_height]];[[_hideMidRight]]'
-								toggles name="[[_midRightName]]"
-								on-click='setMode'
-								disabled='[[_midRightState]]'>[[_midRightText]]</button>
-							<button
-								class='mode'
-								style='[[_rightColor]];min-width:[[_width]];max-width:[[_width]];height:[[_height]];[[_hideRight]]'
-								toggles name="[[_rightName]]"
-								on-click='setMode'
-								disabled='[[_rightState]]'>[[_rightText]]</button>
-						</div>
-					</hui-generic-entity-row>
-		`;
-    }
 
-    static get properties() {
-		return {
-			hass: {
-				type: Object,
-				observer: 'hassChanged'
-			},
-				_config: Object,
-				_stateObj: Object,
-				_modeOff: String,
-				_modeOne: String,
-				_modeTwo: String,
-				_modeThree: String,
-				_width: String,
-				_height: String,
-				_leftColor: String,
-				_midLeftColor: String,
-				_midRightColor: String,
-				_rightColor: String,
-				_leftText: String,
-				_midLeftText: String,
-				_midRightText: String,
-				_rightText: String,
-				_leftName: String,
-				_midLeftName: String,
-				_midRightName: String,
-				_rightName: String,
-				_leftState: Boolean,
-				_midLeftState: Boolean,
-				_midRightState: Boolean,
-				_rightState: Boolean,
-				_hideLeft: Boolean,
-				_hideMidLeft: Boolean,
-				_hideMidRight: Boolean,
-				_hideRight: Boolean,
-		}
-	}
+class CustomFanModeRow extends LitElement {
 
-	setConfig(config) {
-		this._config = config;
-		
+	constructor() {
+		super();
 		this._config = {
 			customTheme: false,
-			sendStateWithMode: false,
+			customSetpoints: false,
 			reverseButtons: false,
-			customModes: false,
-			customText: false,
+			width: '30px',
+			height: '30px',
 			twoModeFan: false,
 			hideOff: false,
+			sendStateWithMode: false,
+			allowDisablingButtons: true,
+			customModes: false,
+			customText: false,
 			modeOff: "none",
 			modeOne: "low",
 			modeTwo: "medium",
 			modeThree: "high",
-			width: '30px',
-			height: '30px',
 			isOffColor: '#f44c09',
 			isOnModeOneColor: '#43A047',
 			isOnModeTwoColor: '#43A047',
@@ -124,14 +40,120 @@ class CustomFanModeRow extends Polymer.Element {
 			customModeOneText: 'LOW',
 			customModeTwoText: 'MED',
 			customModeThreeText: 'HIGH',
-			...config
+			customOffText: 'OFF',
+			customLowText: 'LOW',
+			customMedText: 'MED',
+			customHiText: 'HIGH',
 		};
+	}
+	
+	static get properties() {
+		return {
+			hass: Object,
+			_config: Object,
+			_stateObj: Object,
+			_modeOff: String,
+			_modeOne: String,
+			_modeTwo: String,
+			_modeThree: String,
+			_width: String,
+			_height: String,
+			_leftColor: String,
+			_midLeftColor: String,
+			_midRightColor: String,
+			_rightColor: String,
+			_leftText: String,
+			_midLeftText: String,
+			_midRightText: String,
+			_rightText: String,
+			_leftName: String,
+			_midLeftName: String,
+			_midRightName: String,
+			_rightName: String,
+			_leftState: Boolean,
+			_midLeftState: Boolean,
+			_midRightState: Boolean,
+			_rightState: Boolean,
+			_hideLeft: Boolean,
+			_hideMidLeft: Boolean,
+			_hideMidRight: Boolean,
+			_hideRight: Boolean,
+		};
+	}
+
+	static get styles() {
+		return css`
+			:host {
+				line-height: inherit;
+			}
+			.mode {
+				margin-left: 2px;
+				margin-right: 2px;
+				background-color: #759aaa;
+				border: 1px solid lightgrey; 
+				border-radius: 4px;
+				font-size: 10px !important;
+				color: inherit;
+				text-align: center;
+				float: left !important;
+				padding: 1px;
+				cursor: pointer;
+			}
+		`;
+	}
+
+	render() {
+		return html`
+			<hui-generic-entity-row .hass="${this.hass}" .config="${this._config}">
+				<div id='button-container' class='horizontal justified layout'>
+					<button
+						class='mode'
+						style='${this._leftColor};min-width:${this._width};max-width:${this._width};height:${this._height};${this._hideLeft}'
+						toggles name="${this._leftName}"
+						@click=${this.setMode}
+						.disabled=${this._leftState}>${this._leftText}</button>
+					<button
+						class='mode'
+						style='${this._midLeftColor};min-width:${this._width};max-width:${this._width};height:${this._height};${this._hideMidLeft}'
+						toggles name="${this._midLeftName}"
+						@click=${this.setMode}
+						.disabled=${this._midLeftState}>${this._midLeftText}</button>
+					<button
+						class='mode'
+						style='${this._midRightColor};min-width:${this._width};max-width:${this._width};height:${this._height};${this._hideMidRight}'
+						toggles name="${this._midRightName}"
+						@click=${this.setMode}
+						.disabled=${this._midRightState}>${this._midRightText}</button>
+					<button
+						class='mode'
+						style='${this._rightColor};min-width:${this._width};max-width:${this._width};height:${this._height};${this._hideRight}'
+						toggles name="${this._rightName}"
+						@click=${this.setMode}
+						.disabled=${this._rightState}>${this._rightText}</button>
+				</div>
+			</hui-generic-entity-row>
+		`;
+	}
+
+	firstUpdated() {
+		super.firstUpdated();
+		this.shadowRoot.getElementById('button-container').addEventListener('click', (ev) => ev.stopPropagation());
+	}
+
+	setConfig(config) {
+		this._config = { ...this._config, ...config };
+	}
+
+	updated(changedProperties) {
+		if (changedProperties.has("hass")) {
+			this.hassChanged();
+		}
 	}
 
 	hassChanged(hass) {
 
 		const config = this._config;
-		const stateObj = hass.states[config.entity];
+		const stateObj = this.hass.states[config.entity];
 		const custTheme = config.customTheme;
 		const sendStateWithMode = config.sendStateWithMode;
 		const revButtons = config.reverseButtons;
@@ -292,74 +314,64 @@ class CustomFanModeRow extends Polymer.Element {
 		let m3name = 'mode3'
 		
 		if (revButtons) {
-			this.setProperties({
-				_stateObj: stateObj,
-				_leftState: offstate == 'on',
-				_midLeftState: mode1 === 'on',
-				_midRightState: mode2 === 'on',
-				_rightState: mode3 === 'on',
-				_width: buttonwidth,
-				_height: buttonheight,
-				_leftColor: offcolor,
-				_midLeftColor: mode1color,
-				_midRightColor: mode2color,
-				_rightColor: mode3color,
-				_modeOff: mOff,
-				_modeOne: m1,
-				_modeTwo: m2,
-				_modeThree: m3,
-				_leftText: offtext,
-				_midLeftText: m1text,
-				_midRightText: m2text,
-				_rightText: m3text,
-				_leftName: offname,
-				_midLeftName: m1name,
-				_midRightName: m2name,
-				_rightName: m3name,
-				_hideLeft: hideoff,
-				_hideMidLeft: twomodes_left,
-				_hideMidRight: twomodes_right,
-				_hideRight: nohide,
-			});
+			this._stateObj = stateObj;
+			this._leftState = offstate == 'on';
+			this._midLeftState = mode1 === 'on';
+			this._midRightState = mode2 === 'on';
+			this._rightState = mode3 === 'on';
+			this._width = buttonwidth;
+			this._height = buttonheight;
+			this._leftColor = offcolor;
+			this._midLeftColor = mode1color;
+			this._midRightColor = mode2color;
+			this._rightColor = mode3color;
+			this._modeOff = mOff;
+			this._modeOne = m1;
+			this._modeTwo = m2;
+			this._modeThree = m3;
+			this._leftText = offtext;
+			this._midLeftText = m1text;
+			this._midRightText = m2text;
+			this._rightText = m3text;
+			this._leftName = offname;
+			this._midLeftName = m1name;
+			this._midRightName = m2name;
+			this._rightName = m3name;
+			this._hideLeft = hideoff;
+			this._hideMidLeft = twomodes_left;
+			this._hideMidRight = twomodes_right;
+			this._hideRight = nohide;
 		} else {
-			this.setProperties({
-				_stateObj: stateObj,
-				_leftState: mode3 == 'on',
-				_midLeftState: mode2 === 'on',
-				_midRightState: mode1 === 'on',
-				_rightState: offstate === 'on',
-				_width: buttonwidth,
-				_height: buttonheight,
-				_leftColor: mode3color,
-				_midLeftColor: mode2color,
-				_midRightColor: mode1color,
-				_rightColor: offcolor,
-				_modeOff: mOff,
-				_modeOne: m1,
-				_modeTwo: m2,
-				_modeThree: m3,
-				_leftText: m3text,
-				_midLeftText: m2text,
-				_midRightText: m1text,
-				_rightText: offtext,
-				_leftName: m3name,
-				_midLeftName: m2name,
-				_midRightName: m1name,
-				_rightName: offname,
-				_hideLeft: nohide,
-				_hideMidLeft: twomodes_left,
-				_hideMidRight: twomodes_right,
-				_hideRight: hideoff,
-			});
+			this._stateObj = stateObj;
+			this._leftState = mode3 == 'on';
+			this._midLeftState = mode2 === 'on';
+			this._midRightState = mode1 === 'on';
+			this._rightState = offstate === 'on';
+			this._width = buttonwidth;
+			this._height = buttonheight;
+			this._leftColor = mode3color;
+			this._midLeftColor = mode2color;
+			this._midRightColor = mode1color;
+			this._rightColor = offcolor;
+			this._modeOff = mOff;
+			this._modeOne = m1;
+			this._modeTwo = m2;
+			this._modeThree = m3;
+			this._leftText = m3text;
+			this._midLeftText = m2text;
+			this._midRightText = m1text;
+			this._rightText = offtext;
+			this._leftName = m3name;
+			this._midLeftName = m2name;
+			this._midRightName = m1name;
+			this._rightName = offname;
+			this._hideLeft = nohide;
+			this._hideMidLeft = twomodes_left;
+			this._hideMidRight = twomodes_right;
+			this._hideRight = hideoff;
 		}
-				
-		
 	}
 
-	stopPropagation(e) {
-		e.stopPropagation();
-	}
-	
 	setMode(e) {
 		const mode = e.currentTarget.getAttribute('name');
 		const param = {entity_id: this._config.entity};
